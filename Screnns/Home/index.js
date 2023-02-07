901620b54f85b039fcd66798a516f1508079bd13
Lorrane <lorraner.s@gmail.com>
2023-02-07 13:52:55 -0300

    Home

---
+++ Screnns/Home/index.js
@@ -0,0 +1,63 @@
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import Header from "../../src/components/Header";
+import Balance from "../../src/components/Balance";
+import Moviments from "../../src/components/Moviments";
+import Actions from "../../src/components/Actions";
+
+const list = [
+  {
+    id: 1,
+    label: "Boleto conta Luz",
+    value: "300,00",
+    date: "17/09/2022",
+    type: 0, // despesas
+  },
+  {
+    id: 2,
+    label: "Pix Cliente X",
+    value: "2.500,00",
+    date: "20/09/2022",
+    type: 1, // receitas/ entradas
+  },
+  {
+    id: 3,
+    label: "Salário",
+    value: "7.500,00",
+    date: "22/09/2022",
+    type: 1, // receitas/ entradas
+  },
+];
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header name="João Gomes" />
+      <Balance saldo="9.399,00" gastos={"-850,00"} />
+      <Actions />
+      <Text style={styles.title}>Ultimas movimentações</Text>
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Moviments data={item} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fafafa",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
+  },
+});
